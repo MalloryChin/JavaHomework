55f9fce31f55387df7cc8d07cc66d0357c765c00
MalloryChin <menghsuanchin@gmail.com>
2016-06-14 22:39:15 +0800

    Add files via upload

--- Homework_07_2.java
+++ Homework_07_2.java
@@ -15,13 +15,18 @@ public class Homework_07_2 {
 			while(bis.read() != -1)
 				bytes++;
 			fis.close();
+			bis.close();
 			FileReader fr = new FileReader("Sample.txt");
 			while(fr.read() != -1)
 				chars++;
+			fr.close();
+			fr = new FileReader("Sample.txt");
 			BufferedReader br = new BufferedReader(fr);
 			while(br.readLine() != null)
 				lines++;
 			System.out.printf("Sample.txt檔案共有%d個位元組,%d個字元,%d列資料",bytes,chars,lines);
+			br.close();
+			fr.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
